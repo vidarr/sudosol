7cf9ce17ca9b43c615ceb22d83d8bc46f1d88448
Michael J. Beer <michael.josef.beer@googlemail.com>
2019-07-24 22:07:45 +0000

    Added copyright header to gui.py

--- gui.py
+++ gui.py
@@ -1,3 +1,41 @@
+#!/usr/bin/python3
+#------------------------------------------------------------------------------
+#
+# (C) 2019 Michael J. Beer
+#
+# All rights reserved.
+#
+# Redistribution  and use in source and binary forms, with or with‐
+# out modification, are permitted provided that the following  con‐
+# ditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above  copy‐
+# right  notice,  this  list  of  conditions and the following dis‐
+# claimer in the documentation and/or other materials provided with
+# the distribution.
+#
+# 3.  Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote  products  derived
+# from this software without specific prior written permission.
+#
+# THIS  SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBU‐
+# TORS "AS IS" AND ANY EXPRESS OR  IMPLIED  WARRANTIES,  INCLUDING,
+# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE  ARE  DISCLAIMED.  IN  NO  EVENT
+# SHALL  THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DI‐
+# RECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS IN‐
+# TERRUPTION)  HOWEVER  CAUSED  AND  ON  ANY  THEORY  OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING  NEGLI‐
+# GENCE  OR  OTHERWISE)  ARISING  IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#------------------------------------------------------------------------------
+
 from tkinter import *
 from tkinter.ttk import Separator
 from sudoku  import Field
@@ -130,12 +168,9 @@ def create_gui(root, field=Field(), mode=GuiMode.SOLVE):
         button.wait_variable(var)
         frame.destroy()
 
-
-
 #-------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    # board = RecursiveBoard()
 
     main = create_gui(Tk())
     mainloop()
